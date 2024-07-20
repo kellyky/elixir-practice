676aa6fedf0691afe62ac02bbccff6ef4916a793
Kelly Popko <kelpopko@gmail.com>
2024-07-20 18:45:14 -0500

    Refactor to use multi-clause function

--- leap/lib/year.ex
+++ leap/lib/year.ex
@@ -1,5 +1,6 @@
 defmodule Year do
-  def leap_year?(year) do
-    rem(year, 4) === 0 and (rem(year, 100) != 0 or rem(year, 400) == 0)
-  end
+  def leap_year?(year) when rem(year, 400) === 0, do: true
+  def leap_year?(year) when rem(year, 100) === 0, do: false
+  def leap_year?(year) when rem(year, 4) === 0, do: true
+  def leap_year?(_), do: false
 end
