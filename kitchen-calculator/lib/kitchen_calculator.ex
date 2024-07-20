a960844dba7ae3b7986f46d009eed084d374ab5a
Kelly Popko <kelpopko@gmail.com>
2024-07-20 17:29:55 -0500

    Refactor kitchen calcluator to more idiomatic elixir

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,36 +1,22 @@
 defmodule KitchenCalculator do
-  def get_volume(volume_pair) do
-    elem(volume_pair, 1)
-  end
+  @milliliters_per_cup 240
+  @milliliters_per_fluid_ounce 30
+  @milliliters_per_tablespoon 15
+  @milliliters_per_teaspoon 5
 
-  def to_milliliter(volume_pair) do
-    volume = get_volume(volume_pair)
-    unit = elem(volume_pair, 0)
+  def get_volume(volume_pair), do: elem(volume_pair, 1)
 
-    ml_vol = cond do
-      unit == :milliliter -> volume * 1
-      unit == :cup -> volume * 240
-      unit == :teaspoon -> volume * 5
-      unit == :tablespoon -> volume * 15
-      unit == :fluid_ounce -> volume * 30
-    end
+  def to_milliliter(v = {:milliliter, _}), do: v
+  def to_milliliter({:cup, volume}), do: {:milliliter, volume * @milliliters_per_cup}
+  def to_milliliter({:fluid_ounce, volume}), do: {:milliliter, volume * @milliliters_per_fluid_ounce}
+  def to_milliliter({:tablespoon, volume}), do: {:milliliter, volume * @milliliters_per_tablespoon}
+  def to_milliliter({:teaspoon, volume}), do: {:milliliter, volume * @milliliters_per_teaspoon}
 
-    { :milliliter, ml_vol}
-  end
-
-  def from_milliliter(volume_pair, unit) do
-    ml_vol = get_volume(volume_pair)
-    
-    vol = cond do
-      unit == :milliliter -> ml_vol / 1
-      unit == :cup -> ml_vol / 240
-      unit == :teaspoon -> ml_vol / 5
-      unit == :tablespoon -> ml_vol / 15
-      unit == :fluid_ounce -> ml_vol / 30
-    end 
-
-    { unit, vol }
-  end
+  def from_milliliter((v = {_, _}), :milliliter), do: v
+  def from_milliliter({_, volume}, :cup), do: {:cup, volume / @milliliters_per_cup}
+  def from_milliliter({_, volume}, :fluid_ounce), do: {:fluid_ounce, volume / @milliliters_per_fluid_ounce}
+  def from_milliliter({_, volume}, :tablespoon), do: {:tablespoon, volume / @milliliters_per_tablespoon}
+  def from_milliliter({_, volume}, :teaspoon), do: {:teaspoon, volume / @milliliters_per_teaspoon}
 
   def convert(volume_pair, unit) do
     from_milliliter(to_milliliter(volume_pair), unit)
