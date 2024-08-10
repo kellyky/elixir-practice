b28ef9d2eb726a66dcbca726cad7181e800cc00b
Kelly Popko <kelpopko@gmail.com>
2024-08-10 09:55:12 -0500

    Replace Map.put/3 with Map.update/4 in update_score/3

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -1,10 +1,11 @@
-require IEx
 defmodule HighScore do
+  @initial_score 0
+
   def new() do
     %{}
   end
 
-  def add_player(scores, name, score \\ 0) do
+  def add_player(scores, name, score \\ @initial_score) do
     Map.put_new(scores, name, score)
   end
 
@@ -13,12 +14,11 @@ defmodule HighScore do
   end
 
   def reset_score(scores, name) do
-    Map.put(scores, name, 0)
+    Map.put(scores, name, @initial_score)
   end
 
   def update_score(scores, name, score) do
-    original_score = Map.get(scores, name) || 0
-    Map.put(scores, name, score + original_score)
+    Map.update(scores, name, score, fn current_score -> current_score + score end)
   end
 
   def get_players(scores) do
