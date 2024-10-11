0d7e310daf17701bea4ce19ef9330ea2d4010e32
Kelly Popko <kelpopko@gmail.com>
2024-10-11 01:10:10 -0500

    Iteration 2, Two-Fer, Elixir
    Returns "One for..." if name is_bitstring. TIL you can check for that
    instead of ruling out the others!

--- two-fer/lib/two_fer.ex
+++ two-fer/lib/two_fer.ex
@@ -1,7 +1,4 @@
 defmodule TwoFer do
-  def two_fer(name \\ "you")
-  def two_fer(name) when is_number(name), do: raise FunctionClauseError
-  def two_fer(name) when is_atom(name), do: raise FunctionClauseError
-  def two_fer(name) when is_list(name), do: raise FunctionClauseError
-  def two_fer(name), do: "One for #{name}, one for me."
+  def two_fer(name \\ "you") when is_bitstring(name), do: "One for #{name}, one for me."
+  def two_fer(name), do: raise FunctionClauseError
 end
